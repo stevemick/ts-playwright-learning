09dafd2c5cf44c667be7c78a64e9a98efe25f0f8
stevemick <stevemg99@gmail,com>
2025-12-21 20:57:33 +0530

    Day 1: JavaScript basics using let and const

---
+++ javascript-basics/variables.js
@@ -0,0 +1,11 @@
+// Course details (constants - values will not change)
+const courseName = 'JavaScript';
+const authorName = 'Liam Livingston';
+
+// Knowledge level (can change over time)
+let level = 'Beginner';
+level = 'Intermediate';
+
+console.log('Course Name:', courseName);
+console.log('Author Name:', authorName);
+console.log('Knowledge Level:', level);
